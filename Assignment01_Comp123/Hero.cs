1301111db9ea4778d4ab83398e733bb94657af33
Anson Baby <ansonbm@gmail.com>
2015-01-30 13:04:37 -0500

    Assignment1initial commit

---
+++ Assignment01_Comp123/Hero.cs
@@ -0,0 +1,75 @@
+﻿/*Created by :AnsonBaby
+ * Lastmodified: 27th Jan 2015
+ * Program description: Hero class implimentation 
+ * Revision History: 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01_Comp123
+{
+    class Hero
+    {
+        //Private Properties++++++++++++++++++++++++++++++++++
+        private int strength;
+        private int speed;
+        private int health;
+
+
+        //Public Properties+++++++++++++++++++++++++++++++++
+        public string name;
+        Random rnd = new Random();
+
+        // Constructor Method
+        public Hero(string name)
+        {
+            this.name = name;
+            generateAbilities();
+        }
+
+        // Private Methods+++++++++++++++++++++++++++++++++++
+        // generating hero abilities random number between 1 and 100
+        private void generateAbilities()
+        {
+            strength = rnd.Next(1, 100);
+            speed = rnd.Next(1, 100);
+            health = rnd.Next(1, 100);
+        }
+        //checks hit attempt of 20 % hit chance
+        private bool hitAttempt()
+        {
+            return (rnd.Next(1, 5) == 1);
+        }
+        // Hit damage is hero's strength multipied with random number between 1 and 6
+        private int hitDamage()
+        {
+            return (strength * rnd.Next(1, 6));
+        }
+        
+
+        // Public Methods+++++++++++++++++++++++++++++++++++++++++++++
+
+        public void fight()
+        {
+
+            if (hitAttempt() == true)
+            {
+                int damage = hitDamage();
+                Console.WriteLine("Hit damage is {0}", damage);
+            }
+        }
+
+        public void show()
+        {
+            Console.WriteLine("Strength is {0}", strength);
+            Console.WriteLine("Speed is {0}", speed);
+            Console.WriteLine("Health is {0}", health);
+
+        }
+
+        
+    }
+}
---
+++ Assignment01_Comp123/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Created by :AnsonBaby
+ * Lastmodified: 27th Jan 2015
+ * Program description: Program entry point 
+ * Revision History: 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01_Comp123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero superMan = new Hero("superMan");//new object
+            superMan.show();// call the show method to display the abilities
+            superMan.fight();// call the fight method from the hero class
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();//wait for an entry to exit 
+
+        }
+    }
+}
